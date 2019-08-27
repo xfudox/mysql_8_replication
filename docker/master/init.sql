b38a742d8f10f85471ef4a06466daffc66fe4c14
SILVIOLENOVO\silvi <silvio.gratani@gmail.com>
2019-08-27 21:47:38 +0200

    first commit

---
+++ docker/master/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+	  first_name varchar(25)
+	, last_name varchar(25)
+	, department varchar(15)
+	, email varchar(50)
+);
+
+INSERT INTO employees (first_name, last_name, department, email) 
+VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com');
